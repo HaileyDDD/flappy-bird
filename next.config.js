61dfd6be2a14d7c7bd04885fc24b21b926f78507
Your GitHub Username <your-email@example.com>
2024-11-15 15:43:46 +0800

    准备部署到 Vercel

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // 可选：如果使用新的 App Router 特性
     appDir: true
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = './src';
+    return config;
   }
 }
 
